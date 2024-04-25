82e207196714358c3d640a5874e8b14b0934e858
Aadarsh Unni Wilson <36239301+waadarsh@users.noreply.github.com>
2024-04-25 14:51:34 +0530

    initial commit

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    uname = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String)
+
+Base.metadata.create_all(bind=engine)
