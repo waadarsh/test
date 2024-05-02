7040eca22e51725565e9e8cc691f75ee8dc0811d
Aadarsh Unni Wilson <36239301+waadarsh@users.noreply.github.com>
2024-05-02 14:50:49 +0530

    Update AdminPage.js

--- AdminPage.js
+++ AdminPage.js
@@ -1,12 +1,26 @@
 // src/AdminPage.js
-import React from 'react';
+import React, {useState} from 'react';
+import {Container} from 'reactstrap';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
 
 const AdminPage = ({ user, logout }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen)
+
+  if(!user || !user.uname){
+    return <div>Loading user data, please wait...</div>
+  }
+  
   return (
-    <div>
-      <h2>Admin Dashboard</h2>
-      <p>Welcome, {user.uname}. You have administrative access.</p>
-      <button onClick={logout}>Logout</button>
+    <div style={{display:'flex'}}>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Container fluid style={{ marginLeft: isOpen ? '250px' : '50px', transition: 'margin-left 0.3s', paddingLeft: '20px' }}>
+        <Topbar />
+        <h2>Admin Dashboard</h2>
+        <p>Welcome, {user.uname}. You have administrative access.</p>
+        <button onClick={logout}>Logout</button>
+      </Container>
     </div>
   );
 };
