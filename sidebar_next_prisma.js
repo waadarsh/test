c08b76d270ebf794fa5f3c13adb497e0aa350800
Aadarsh Unni Wilson <36239301+waadarsh@users.noreply.github.com>
2024-05-06 15:09:10 +0530

    Create sidebar_next_prisma.js

---
+++ sidebar_next_prisma.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+
+const Sidebar = ({ isOpen, logout }) => {
+    const router = useRouter();
+
+    return (
+        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+            <Nav vertical>
+                <NavItem>
+                    <NavLink href="#" onClick={() => router.push('/admin/assignment-request')}>
+                        Assignment Request
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#" onClick={() => router.push('/admin/reports')}>
+                        Reports
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="#" onClick={logout}>
+                        Logout
+                    </NavLink>
+                </NavItem>
+            </Nav>
+        </div>
+    );
+};
+
+export default Sidebar;
